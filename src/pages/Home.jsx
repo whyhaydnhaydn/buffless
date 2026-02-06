5aaebb85b3b019b58f65ddb676ca676c12b63908
Haydn P <whyhaydn@gmail.com>
2026-02-06 02:02:23 +0000

    File changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useQuery } from '@tanstack/react-query';
 import { base44 } from '@/api/base44Client';
+import { motion } from 'framer-motion';
 
 import HeroSection from '../components/home/HeroSection';
 import TrustedBrands from '../components/home/TrustedBrands';
